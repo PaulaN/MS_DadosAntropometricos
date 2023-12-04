8e23601c1d6f7c38e25fc9943c9945c0fa6e1e73
PaulaN <paula_ndesousa@hotmail.com>
2023-12-04 18:09:51 -0300

    inclusao do fluxo de medidas objetivo

---
+++ DadosAntropometricos/BLL/MedidasObjetivoBLL.cs
@@ -0,0 +1,62 @@
+﻿using AcompanhamentoFisico.DTO;
+using DadosAntropometricos.DAO;
+
+namespace DadosAntropometricos.BLL
+{
+    public class MedidasObjetivoBLL
+    {
+        int retornoMedidas;
+
+        MedidasObjetivoDAO dao = new MedidasObjetivoDAO();
+        public String insereMedidasObjetivo(MedidasDTO medidasDTO, String CPF)
+        {
+            retornoMedidas = 0;
+
+            retornoMedidas = dao.insereMedidasObjetivo(medidasDTO, CPF);
+
+
+            return retornoMedidas == 1 ? "Cadastro realizado com sucesso" : "Não foi possível cadastrar medidas objetivo";
+        }
+
+        public MedidasDTO retornaMedidasObjetivoPorCPF(String CPF)
+        {
+
+            MedidasDTO medidasDTO = dao.retornaMedidasObjetivo(CPF);
+
+            return medidasDTO;
+        }
+        public String deletaMedidasObjetivo(String CPF)
+        {
+            String retorno = "";
+            int retornoMedidas = 0;
+
+
+            if (CPF != null)
+            {
+                retornoMedidas = dao.deletaMedidasObjetivo(CPF);
+            }
+
+            if (retornoMedidas == 1)
+            {
+                retorno = "Deletado com sucesso";
+            }
+            else
+            {
+                retorno = "Não foi possível deletado medidas objetivo";
+            }
+
+
+            return retorno;
+        }
+
+        public String alteraMedidasObjetivo(MedidasDTO medidasDTO, String CPF)
+        {
+            retornoMedidas = 0;
+
+            retornoMedidas = dao.alteraMedidasObjetivo(medidasDTO, CPF);
+
+            return retornoMedidas == 1 ? "Alteração realizada com sucesso" : "Não foi possível alterar medidas objetivo";
+        }
+    }
+
+}
---
+++ DadosAntropometricos/Controllers/MedidasObjetivosController.cs
@@ -0,0 +1,55 @@
+﻿using AcompanhamentoFisico.BLL;
+using AcompanhamentoFisico.DTO;
+using DadosAntropometricos.BLL;
+using Microsoft.AspNetCore.Mvc;
+
+// For more information on enabling Web API for empty projects, visit https://go.microsoft.com/fwlink/?LinkID=397860
+
+namespace DadosAntropometricos.Controllers
+{
+    [Route("api/[controller]")]
+    [ApiController]
+    public class MedidasObjetivosController : ControllerBase
+    {
+        MedidasObjetivoBLL bll = new MedidasObjetivoBLL();
+
+        [HttpGet("{CPF}")]
+        public MedidasDTO BuscaPorCPF(String CPF)
+        {
+
+            MedidasDTO medidasDTO = bll.retornaMedidasObjetivoPorCPF(CPF);
+
+
+
+            return medidasDTO;
+        }
+
+        [HttpPost]
+        public String Post(MedidasDTO medidasDTO, String CPF)
+        {
+            String retorno = bll.insereMedidasObjetivo(medidasDTO, CPF);
+
+            return retorno;
+
+        }
+
+
+        [HttpDelete("{CPF}")]
+        public String Delete(String CPF)
+        {
+            String retorno = bll.deletaMedidasObjetivo(CPF);
+
+            return retorno;
+        }
+
+
+
+        [HttpPut]
+        public String Put(MedidasDTO medidasDTO, String CPF)
+        {
+            String retorno = bll.alteraMedidasObjetivo(medidasDTO, CPF);
+
+            return retorno;
+        }
+    }
+}
